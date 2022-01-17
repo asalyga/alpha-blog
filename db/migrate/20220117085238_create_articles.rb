3e3779a1b02d3fa1c73a779cd4243e767b837fbc
asalyga <aleksander.salyga@gmail.com>
2022-01-17 10:14:53 +0100

    Added articles model

---
+++ db/migrate/20220117085238_create_articles.rb
@@ -0,0 +1,9 @@
+# Articles creation
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+    end
+  end
+end
---
+++ db/migrate/20220117091116_add_timestamps_to_articles.rb
@@ -0,0 +1,7 @@
+# Adding timestamps to Articles
+class AddTimestampsToArticles < ActiveRecord::Migration[6.0]
+  def change
+    add_column :articles, :created_at, :datetime
+    add_column :articles, :updated_at, :datetime
+  end
+end
