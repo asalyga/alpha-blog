e3b3b46ead21af239aa1842519e7b131206f1c63
asalyga <aleksander.salyga@gmail.com>
2022-02-23 23:03:10 +0100

    Add login/logout view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,22 @@
 class SessionsController < ApplicationController
 
   def new; end
-  def create
-    byebug
-  end
-  def destroy; end
 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged in successfully'
+      redirect_to user
+    else
+      flash.now[:alert] = 'There was a problem with login details'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to root_path
+  end
 end
