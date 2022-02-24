31afe174c63596c009f21da7dc6a76276df1a8c5
asalyga <aleksander.salyga@gmail.com>
2022-02-24 14:47:58 +0100

    Fix rubocop offences

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   end
 
   private
-  
+
   def set_article
     @article = Article.find(params[:id])
   end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,6 @@ class StaticPagesController < ApplicationController
   def home
     redirect_to articles_path if logged_in?
   end
+
   def about; end
 end
