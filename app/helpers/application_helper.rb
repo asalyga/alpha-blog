41f374a6cb468d67053d49f50fe32aeb7dfec6fc
asalyga <aleksander.salyga@gmail.com>
2022-02-24 14:46:27 +0100

    Move helper methods to App_controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,8 @@
 module ApplicationHelper
-
   def gravatar_for(user)
     email_address = user.email
     hash = Digest::MD5.hexdigest(email_address)
     gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
-
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    !!current_user
-  end
-  
 end
